b03d210398bc2a7ef5162cf21fa27301d3296cfd
Chris Basham <chris@bash.am>
2023-07-05 16:32:27 -0400

    Add backup script

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import config from 'config';
 import fs from 'node:fs';
 import { createRequire } from 'node:module';
 import ws from 'ws';
-// import * as Y from 'yjs';
 
 if (!fs.existsSync(config.dbDir)) {
 	fs.mkdirSync(config.dbDir, { recursive: true });
@@ -13,13 +12,10 @@ process.env.YPERSISTENCE = config.dbDir;
 const require = createRequire(import.meta.url);
 const { WebsocketProvider } = require('y-websocket');
 const { getYDoc } = require('y-websocket/bin/utils');
-const { docPrefix = '' } = config;
 
 config.docs.forEach(syncDoc);
 
-function syncDoc (id) {
-	const name = `${docPrefix}${id}`;
-	// const doc = new Y.Doc();
+function syncDoc (name) {
 	const doc = getYDoc(name);
 	const provider = new WebsocketProvider(
 		config.server,
